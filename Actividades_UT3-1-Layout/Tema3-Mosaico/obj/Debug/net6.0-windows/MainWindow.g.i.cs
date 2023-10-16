717430e88bb47ca14ce4c34a523d9a1cf86be839
AINARA SERNA MÉNDEZ <ainsermen@alu.edu.gva.es>
2023-10-16 12:10:39 +0200

    Ejercicio 'Visor de imágenes' casi hecho

--- Actividades_UT3-1-Layout/Tema3-Mosaico/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Actividades_UT3-1-Layout/Tema3-Mosaico/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Tema3_Mosaico {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Tema3-Mosaico;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Tema3-Mosaico;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
